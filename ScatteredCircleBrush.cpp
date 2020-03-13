0c520e49fd1598192782c23b7c497bc51755ac85
David Tang <davidtang1006@gmail.com>
2020-03-13 16:03:02 +0800

    Add scattered circle brush.

---
+++ ScatteredCircleBrush.cpp
@@ -0,0 +1,65 @@
+//
+// ScatteredCircleBrush.cpp
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "ScatteredCircleBrush.h"
+#include <math.h>
+
+extern float frand();
+
+ScatteredCircleBrush::ScatteredCircleBrush(ImpressionistDoc* pDoc, char* name) : ImpBrush(pDoc, name)
+{
+}
+
+void ScatteredCircleBrush::BrushBegin(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	BrushMove(source, target);
+}
+
+void ScatteredCircleBrush::BrushMove(Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("ScatteredCircleBrush::BrushMove document is NULL\n");
+		return;
+	}
+
+	int size = pDoc->getSize();
+	float alpha = dlg->getAlpha();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	for (int i = 0; i < 3; i++) {
+		float randomMovementX = (frand() - 0.5f) * 3 * size;
+		float randomMovementY = (frand() - 0.5f) * 3 * size;
+
+		glPushMatrix();
+		glTranslatef(target.x + randomMovementX, target.y + randomMovementY, 0);
+		glBegin(GL_POLYGON);
+
+		source.translate(randomMovementX, randomMovementY);
+		SetColor(source, alpha);
+		source.translate(-randomMovementX, -randomMovementY); // Revert the translation
+		int circlePoints = 100;
+		for (int i = 0; i < circlePoints; i++) {
+			float angle = 2 * M_PI * i / circlePoints;
+			glVertex2f(size * cos(angle), size * sin(angle));
+		}
+
+		glEnd();
+		glPopMatrix();
+	}
+}
+
+void ScatteredCircleBrush::BrushEnd(const Point source, const Point target)
+{
+	// do nothing so far
+}
